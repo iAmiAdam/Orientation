b13d4362c7b36a596e573a619fe5d0d3fbf46e44
Adam Smith <adam.smith18@ntlworld.com>
2014-06-08 19:31:26 +0100

    Adding a basic menu

--- src/info/adamjsmith/orientations/OrientationsActivity.java
+++ src/info/adamjsmith/orientations/OrientationsActivity.java
@@ -3,7 +3,9 @@ package info.adamjsmith.orientations;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.app.ActionBar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class OrientationsActivity extends Activity {
 
@@ -12,9 +14,35 @@ public class OrientationsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orientations);
         Log.d("StateInfo", "onCreate");
-        
-        ActionBar actionBar = getActionBar();
-        actionBar.hide();
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	CreateMenu(menu);
+    	return true;
+    }
+    
+    @Override 
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	return MenuChoice(item);
+    }
+    
+    private void CreateMenu(Menu menu) {
+    	MenuItem mnu1 = menu.add(0,0,0,"Item 1");
+    	{
+    		mnu1.setIcon(R.drawable.ic_launcher);
+    		mnu1.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+    	}
+    }
+    
+    private boolean MenuChoice(MenuItem item) {
+    	switch (item.getItemId()) {
+    	case 0:
+    		Toast.makeText(this, "You clicked on Item 1", Toast.LENGTH_LONG).show();
+    		return true;
+    	}
+    	return false;
     }
     
     @Override
